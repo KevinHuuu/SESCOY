2f95f258e3db689449f54f21d05cd7eb16fc342a
KevinHuuu <hcr14@mails.tsinghua.edu.cn>
2020-04-24 01:20:18 -0700

    new quote test

--- src/test/java/com/circleci/demojavaspring/FastTest.java
+++ src/test/java/com/circleci/demojavaspring/FastTest.java
@@ -1,6 +1,8 @@
 package com.circleci.demojavaspring;
 
 import static org.junit.Assert.*;
+
+import com.circleci.demojavaspring.model.Quote;
 import org.junit.Test;
 
 public class FastTest {
@@ -8,6 +10,12 @@ public class FastTest {
     @Test
     public void fastTest() throws InterruptedException {
         assertTrue(true);
+    }
+    @Test
+    public void newQuotesTest() throws InterruptedException {
+        Quote quote = new Quote();
+        quote.setQuote("Hey");
+        assertEquals(quote.getQuote(), "Heyy");
     }
 
 }
